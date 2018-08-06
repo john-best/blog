6c7d4b28772ed17744e4aa6c3320951a11c9c697
John Ooi <pwnerjohn@gmail.com>
2018-08-05 23:26:30 -0700

    feat: login frontend wip

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Container,
+  InputGroup,
+  Input
+} from "reactstrap";
 
 class Login extends Component {
   render() {
     return (
-        <h1> Help </h1>
-    );
+        <Container>
+        <Card body className="text-center">
+          <CardHeader><span style={{fontSize: '200%'}}>Login</span></CardHeader>
+          <CardBody style={{paddingBottom: 0}}>
+            <InputGroup>
+            <Input placeholder="Email" />
+            </InputGroup>
+            <InputGroup>
+            <Input placeholder="Password" />
+            </InputGroup>
+            <Button style={{marginRight: '0.25%', marginTop: '2%'}}>
+              Login
+            </Button>
+            <Button style={{marginLeft: '0.25%', marginTop: '2%'}}>
+              Register
+            </Button>
+          </CardBody>
+          </Card>
+          </Container>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./components/index.css";
 import App from "./components/App";
 import registerServiceWorker from "./components/registerServiceWorker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
